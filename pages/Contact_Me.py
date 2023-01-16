822b7513e4ccc695148a6612336d9d3db51c2fbc
rancy <chadharancy@gmail.com>
2023-01-16 18:27:56 -0500

    Updated send email

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
+with st.form(key="messageformid"):
+    UserEmail = st.text_input("Your Email Address")
+    rawMsg = st.text_area("Your Message:")
+    button = st.form_submit_button("Submit")
+    UserMsg = f'''\
+Subject: New email from {UserEmail}
+
+From: {UserEmail}
+{rawMsg}
+'''
+    if button:
+        send_email(UserMsg)
+        st.info("Your message is sent!")
