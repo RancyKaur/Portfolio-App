b08771258efd28a64d5263e91ccaaaaf9df0ad9b
rancy <chadharancy@gmail.com>
2023-01-16 20:16:11 -0500

    update app email

--- send_email.py
+++ send_email.py
@@ -2,12 +2,11 @@ import smtplib
 import ssl,os
 
 def send_email(message):
-    useremail="chadharancy@gmail.com"
-    password = os.getenv("MYAPPPWD")
-    print("The pwd is", password)
+    useremail="iamrancy@gmail.com"
+    password = "goyynobxosacaasu" #os.getenv("MYAPPPWD")
     host = "smtp.gmail.com"
     port = 465
-    receiver1 = "chadharancy@gmail.com"
+    receiver1 = "iamrancy@gmail.com"
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
