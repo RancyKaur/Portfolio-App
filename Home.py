9aadfce9b550ba011a123b04845054e035aa971d
rancy <chadharancy@gmail.com>
2023-01-16 21:11:03 -0500

    update app email

--- Home.py
+++ Home.py
@@ -29,13 +29,13 @@ col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
 projects_df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in projects_df[:2].iterrows():
+    for index, row in projects_df[:4].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 with col4:
-    for index, row in projects_df[2:].iterrows():
+    for index, row in projects_df[4:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
